2a011bdd9f52a892f6ca6b568742c30effe0562b
Darius Mejeras <darius.mejeras@gmail.com>
2020-06-11 22:47:39 +0300

    Fix middleware chaining

--- middleware.go
+++ middleware.go
@@ -8,17 +8,15 @@ import (
 type Middleware func(next Handle) Handle
 
 func chainMiddleware(handle Handle, mw ...Middleware) Handle {
-	return func(w http.ResponseWriter, rq *http.Request, p httprouter.Params) error {
-		if handle == nil {
-			handle = func(w http.ResponseWriter, r *http.Request, p httprouter.Params) error {
-				return nil
-			}
+	if handle == nil {
+		handle = func(w http.ResponseWriter, r *http.Request, p httprouter.Params) error {
+			return nil
 		}
-
-		for i := range mw {
-			handle = mw[len(mw)-1-i](handle)
-		}
-
-		return handle(w, rq, p)
 	}
+
+	for i := range mw {
+		handle = mw[len(mw)-1-i](handle)
+	}
+
+	return handle
 }
