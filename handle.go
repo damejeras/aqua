f8c17240f9b59c013a5882b77118420e31122bc4
Darius Mejeras <darius.mejera@tesonet.com>
2020-07-07 14:53:09 +0300

    Add params parsing

--- handle.go
+++ handle.go
@@ -2,16 +2,9 @@ package aqua
 
 import (
 	"net/http"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 // Handle is a function that can be registered to a route to handle HTTP
 // requests. Like http.HandlerFunc, but has a third parameter for the values of
 // wildcards (variables) and returns an error to ba handled by framework
 type Handle func(http.ResponseWriter, *http.Request, Params) error
-
-// Params wraps httprouter.Params struct just to keep API consistent
-type Params struct {
-	httprouter.Params
-}
---
+++ params.go
@@ -0,0 +1,54 @@
+package aqua
+
+import (
+	`github.com/julienschmidt/httprouter`
+	`strconv`
+	`fmt`
+)
+
+// Params wraps httprouter.Params and provides additional param type conversion methods
+type Params struct {
+	httprouter.Params
+}
+
+func (p Params) Int(name string) (int, error) {
+	v := p.ByName(name)
+	if v == "" {
+		return 0, ErrMissingParam
+	}
+
+	i, err := strconv.Atoi(v)
+	if err != nil {
+		return 0, ErrInvalidParamType
+	}
+
+	return i, nil
+}
+
+func (p Params) String(name string) (string, error) {
+	v := p.ByName(name)
+	if v == "" {
+		return "", ErrMissingParam
+	}
+
+	return v, nil
+}
+
+func (p Params) Float64(name string) (float64, error) {
+	v := p.ByName(name)
+	if v == "" {
+		return 0, ErrMissingParam
+	}
+
+	f, err := strconv.ParseFloat(v, 64)
+	if err != nil {
+		return 0, ErrInvalidParamType
+	}
+
+	return f, nil
+}
+
+var (
+	ErrInvalidParamType = fmt.Errorf("unexpected param type")
+	ErrMissingParam     = fmt.Errorf("missing param")
+)
